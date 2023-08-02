037f826056a99fff47b490d5a2da6adc67113800
Lewis Ferguson <lewis.h.ferguson@btinternet.com>
2023-08-02 10:45:16 +0100

    added a read me file

---
+++ readme.md
@@ -0,0 +1,20 @@
+-Title Bucket List Travels
+
+The aim of the website is to allow users to view different countries and find out which cities they contain, and also allow the user to add them to their bucket list, and check off which destinations have been visited, and which ones are still to be visited.
+
+The user can create a country and add it to the list of countries on the destinations page. Each country can be edited, and the cities within the countries can be added or removed.
+
+On the bucket list page, they can choose from the list of countries that are available on the destinations page and add them to a visited list or not visited list.
+
+
+PLANNING -
+
+I started off by drawing class diagrams to ensure I had an understanding of the relationships between the classes.
+
+![class diagram](/static/images/class_diagrams.jpg)
+
+I then moved on to drawing a wireframe for each page to get a rough idea of how I wanted my website to look, as you can see, as I have worked on my website I have made some changes to the original plan.
+
+![home wireframe](/static/images/home_wireframe.jpg)
+![destinations wireframe](/static/images/destinations_wireframe.jpg)
+![bucket list wireframe](/static/images/bucket_list_wireframe.jpg)
--- seed.py
+++ seed.py
@@ -15,10 +15,14 @@ def seed():
     country2 = Country(name="England")
     country3 = Country(name="Turkey")
     country4 = Country(name="Thailand")
+    country5 = Country(name="Italy")
+    country6 = Country(name="Wales")
     db.session.add(country1)
     db.session.add(country2)
     db.session.add(country3)
     db.session.add(country4)
+    db.session.add(country5)
+    db.session.add(country6)
     db.session.commit()
     
     city1 = City(name="Edinburgh", country_id = country1.id)
@@ -29,6 +33,10 @@ def seed():
     city6 = City(name="Phuket" , country_id = country4.id)
     city7 = City(name="Antalya", country_id = country3.id)
     city8 = City(name="Manchester", country_id = country2.id)
+    city9 = City(name="Rome", country_id = country5.id)
+    city10 = City(name="Venice", country_id = country5.id)
+    city11 = City(name="Cardiff", country_id = country6.id)
+    city12 = City(name="Swansea", country_id = country6.id)
 
     db.session.add(city1)
     db.session.add(city2)
@@ -38,6 +46,10 @@ def seed():
     db.session.add(city6)
     db.session.add(city7)
     db.session.add(city8)
+    db.session.add(city9)
+    db.session.add(city10)
+    db.session.add(city11)
+    db.session.add(city12)
 
  
     db.session.commit()
